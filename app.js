5714df323e19e5543d3663cb79caa6f916a18d4b
gamejam-sandwich <pinkelephantloopsy@gmail.com>
2025-08-18 00:00:49 -0700

    modified:   app.js 	new file:   images/hmm.jpg 	new file:   images/kermit.jpg 	new file:   images/steve.jpg 	modified:   index.html 	modified:   style.css

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// Selectors
+/*// Selectors
 const questionDiv = document.querySelector('.question');
 const optionsDiv = document.querySelector('.options');
 const startBtn = document.querySelector('.start');
@@ -80,11 +80,54 @@ function replay() {
     currentIndex = 0;
     location.reload();
 }
+*/
 
+// Selectors
+const boxDiv = document.querySelector('.quiz-area');
+const imageDiv = document.querySelector('.the-object');
+const zoomImg = document.querySelector('.zoom-image');
+const optionsDiv = document.querySelector('.options');
+const startBtn = document.querySelector('.start');
 
-const zoomBtn = document.querySelector('.zoom');
-const zoomImg = document.querySelector('.doggo');
+// Variables
+let currentIndex = 0;
+let score = 0;
+const imgData = [
+    {
+        imgSrc: 'images/steve.jpg',
+        options: ['Sky', 'Ocean', 'Hedgehog', 'Jello'],
+        answer: 'Hedgehog',
+    },
+    {
+        imgSrc: 'images/dog.jpg',
+        options: ['Q-Tip', 'Pillow', 'Dog', 'Rug'],
+        answer: 'Dog',
+    },
+    {
+        imgSrc: 'images/kermit.jpg',
+        options: ['Frog', 'Sock', 'T-Shirt', 'Grass'],
+        answer: 'Frog',
+    }
+];
 
+startBtn.addEventListener('click', showImage);
+
+// Functions
+function showImage() {
+    startBtn.style.display = 'none';
+    zoomImg.style.scale = 5;
+    const currentImage = imgData[currentIndex];
+    zoomImg.innerHTML = currentImage.imgSrc;
+
+    currentImage.options.forEach(option => {
+        const optionBtn = document.createElement('button');
+        optionBtn.innerText = option;
+        boxDiv.appendChild(optionBtn);
+        optionBtn.addEventListener('click', selectAnswer);
+    })
+}
+
+// Animation stuff
 const restoreZoom = [
     { scale: 5 },
     { scale: 1 },
@@ -95,6 +138,42 @@ const zoomTiming = {
     iterations: 1,
 }
 
-zoomBtn.addEventListener('click', () => {
-    zoomImg.animate(restoreZoom, zoomTiming);
-});
+// Choosing an answer
+function selectAnswer(event) {
+    const selectedBtn = event.target;
+    const answer = imgData[currentIndex].answer;
+
+    const animation = zoomImg.animate(restoreZoom, zoomTiming);
+    animation.onfinish = () => {
+        zoomImg.style.scale = 1;
+    };
+
+    if (selectedBtn.innerText === answer) {
+        score++;
+    }
+
+    currentIndex++;
+    if (currentIndex < imgData.length) {
+        showImage();
+    } else {
+        showScore();
+    }
+}
+
+// End of game
+function showScore() {
+    gameDiv.innerHTML = `
+    <h1>Game Finished!</h1>
+    <p>Your Score: ${score}/${imgData.length}
+    `;
+    const restartBtn = document.createElement('button');
+    restartBtn.innerText = 'Play again?';
+    gameDiv.appendChild(restartBtn);
+    restartBtn.addEventListener('click', replay);
+}
+
+function replay() {
+    score = 0;
+    currentIndex = 0;
+    location.reload();
+}
