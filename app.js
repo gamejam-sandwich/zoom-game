29131c28d47472f4e6848dd99d72f2c1be509ca0
gamejam-sandwich <pinkelephantloopsy@gmail.com>
2025-08-29 10:40:55 -0700

    modified:   app.js 	modified:   style.css

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 // Upper Screen selectors
-const upperScreen = document.querySelector('upper-screen');
+const upperScreen = document.querySelector('.upper-screen');
 const title = document.querySelector('.title');
 const captions = document.querySelector('.captions');
 // Lower Screen
-const lowerScreen = document.querySelector('quiz-screen')
-const optionsDiv = document.querySelector('options-div');
-const startBtn = document.querySelector('start-button');
-const nextBtn = document.querySelector('next-button');
+const lowerScreen = document.querySelector('.quiz-screen')
+const optionsDiv = document.querySelector('.options-div');
+const startBtn = document.querySelector('.start-button');
+const nextBtn = document.querySelector('.next-button');
+
+// Variables
+let index = 0;
+let score = 0;
+
+// Image data
+//Use JSON file and fetch
+//Readable stream
+//Big array
 
 startBtn.addEventListener('click', startGame);
+
 function startGame() {
-    alert('ui');
-    nextBtn.style.display = 'none';
+    alert('game start');
 }
