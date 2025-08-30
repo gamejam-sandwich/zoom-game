865970a6a8abc05c2a098b50637e180609bcb79c
gamejam-sandwich <pinkelephantloopsy@gmail.com>
2025-08-30 11:46:25 -0700

    Created option buttons, implemented zoom animation, general prevention of bugs/exploits

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ import foods from "./foodList.js";
 
 // Upper Screen selectors
 const upperScreen = document.querySelector('.upper-screen');
-const title = document.querySelector('.title');
 const captions = document.querySelector('.captions');
 const imagePrompt = document.querySelector('.image-prompt');
 
@@ -15,7 +14,11 @@ const nextBtn = document.querySelector('.next-button');
 
 // Variables
 let score = 0;
+let answerIndex = 0;
+let randomNumber = Math.floor(Math.random() * 58);
+let answers = [];
 let prompts = [];
+
 
 startBtn.addEventListener('click', startGame);
 nextBtn.addEventListener('click', setImage);
@@ -24,20 +27,26 @@ nextBtn.style.display = 'none';
 // Functions
 function startGame() {
     // Generate list of 5 random food items
-    let i = 0;
-    while (i < 5) {
+    while (prompts.length < 5) {
         // Pick 5 random numbers from 57 total options
-        let randomNumber = Math.floor(Math.random() * 58);
         let randomPrompt = `images/foods/item_Food${foods[randomNumber][0]}.png`;
+        // Regenerate if duplicates present
+        while (prompts.includes(randomPrompt)) {
+            randomNumber = Math.floor(Math.random() * 58);
+            randomPrompt = `images/foods/item_Food${foods[randomNumber][0]}.png`;
+        }
         prompts.push(randomPrompt);
-        i++;
+        answers.push(randomNumber);
     }
     setImage();
     startBtn.style.display = 'none';
-    nextBtn.style.display = 'block';
 }
 
 function setImage() {
+    nextBtn.style.display = 'none';
+    captions.innerText = 'What is this object?';
+    imagePrompt.style.scale = 5;
+
     // Once all 5 have been shown, game ends
     if (prompts.length == 0) {
         showScore();
@@ -45,8 +54,75 @@ function setImage() {
     }
     // Set imagePrompt to first image option
     imagePrompt.src = prompts[0];
-    // Remove prompt from array
+    // Remove used prompt from array
     prompts.shift();
+
+    // Generating 2 random options (predetermined option is the answer)
+    optionsDiv.innerHTML = '';
+    optionsDiv.className = 'options-div';
+
+    let optionsArray = [answers[0]];
+    answerIndex = answers[0];
+    answers.shift();
+    for (let i = 0; i < 2; i++) {
+        // Again, regen if dupes present
+        while (optionsArray.includes(randomNumber)) {
+            randomNumber = Math.floor(Math.random() * 58);
+        }
+        optionsArray.push(randomNumber);
+        shuffle(optionsArray);
+    }
+
+    // Create 3 buttons with values from optionsArray
+    for (let i = 0; i < optionsArray.length; i++) {
+        const optionBtn = document.createElement('button');
+        let randomOption = foods[optionsArray[i]][1];
+        optionBtn.innerText = randomOption;
+        optionBtn.className = 'option-button';
+        optionsDiv.appendChild(optionBtn);
+        optionBtn.addEventListener('click', checkAnswer);
+    }
+}
+
+/* Randomize the order of the array,
+*  ensuring the answer option does not
+*  remain in the same location
+*/
+function shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
+}
+
+// Animations
+const restoreZoom = [
+    { scale: 5 },
+    { scale: 1 }
+];
+
+const zoomTiming = {
+    duration: 1000,
+    iterations: 1
+}
+
+// Verify if selected option is correct
+function checkAnswer(event) {
+    const selectedOption = event.target;
+    let answer = foods[answerIndex][1];
+    // Prevent user from spamming answer buttons
+    optionsDiv.className = 'disabled-btns';
+
+    // Zoom out to reveal answer, update score
+    const animation = imagePrompt.animate(restoreZoom, zoomTiming);
+    animation.onfinish = () => {
+        imagePrompt.style.scale = 1;
+        if (selectedOption.innerText === answer) {
+            captions.innerText = 'Correct!';
+            score++;
+        }
+        else {
+            captions.innerText = `Womp womp. It was ${answer}.`;
+        }
+        nextBtn.style.display = 'block';
+    };
 }
 
 function showScore() {
@@ -54,6 +130,8 @@ function showScore() {
     upperScreen.innerHTML = `yahoo${score}`;
     // Replay button on lower screen
     nextBtn.style.display = 'none';
+    optionsDiv.innerHTML = '';
+
     const replayBtn = document.createElement('button');
     replayBtn.innerText = 'Play again?';
     replayBtn.className = 'start-button';
